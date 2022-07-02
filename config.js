0d11b013165f5bba1f0dc719853bd4d3d685dab5
vpdp30pttt <108589721+vpdp30pttt@users.noreply.github.com>
2022-07-03 02:15:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Phan Thảo ơii!',
+    introDesc: `Tao có điều này muốn hỏi, nhớ trả lời thật lòng nhaa !!`,
+    btnIntro: 'Oke nè ^^',
+    title: 'Phan Thảo còn thương tao khôngg?🤭 😙',
+    desc: 'Không trả lời hoặc là thoát ra, tức là còn thương á nha ^^ ',
+    btnYes: 'Thương ơi là thương  <33',
+    btnNo: 'Không, nằm mơ à :3',
+    question:'Sao vẫn còn thương vậyyyy ta <3 ',
+    btnReply: 'Gửi cho Phụng <3',
+    reply: 'Ngày trước thương bây giờ cũng thương<33333333',
+    mess: 'Tao biết mà 🥰. Thương Phan Thảo nhìu nhìu😘😘',
+    messDesc: 'Thứ 2 rảnh tao dẫn Thảo đi ăn nhé.!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
